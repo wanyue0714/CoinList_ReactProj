681fd41b039938f4ec92067d0ef162aa11213f30
万悦 <wanyue@banetsus-MBP.fios-router.home>
2018-10-21 08:41:40 -0400

    add app bar

--- src/App.js
+++ src/App.js
@@ -7,25 +7,49 @@ const GreenElement = styled.div`
   font-size: 30px;
   font-family: 'Mali', cursive;
 `;
+// extend the green element
 const BlueElement = styled(GreenElement)`
   color: grey;
 `;
 
-// extend the green element
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const ControlButton = styled.div`
+  font-size: 1.5em;
+`;
+
+const AppLayout = styled.div`
+  padding: 40px;
+  display: grid;
+  grid-template-columns: 400px auto 200px 100px;
+`;
 
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <AppLayout>
+        <Logo>
+           YueYue's home page
+        </Logo>
+        <div>
+        </div>
+        <ControlButton>
+           Dashboard
+        </ControlButton>
+          <ControlButton>
+              Settings
+          </ControlButton>
       <GreenElement>
         Hello, I'm little piggy
       </GreenElement>
       <BlueElement>
        Love you !
       </BlueElement>
-      </div>
+      </AppLayout>
     );
   }
 }
