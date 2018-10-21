bf6acfa65f7dbc2d43b21d9ac7c3b0ba81129b16
万悦 <wanyue@banetsus-MBP.fios-router.home>
2018-10-21 08:18:43 -0400

    Add Styled Components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const GreenElement = styled.div`
+  color: green;
+  font-size: 30px;
+`;
+const BlueElement = styled(GreenElement)`
+  color: blue;
+`;
+
+// extend the green element
+
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-           <p>
-            Edit <code>src/App.js</code> and save to reload.
-           </p>
-           <p>
-            Welcome to yueyue's page!
-           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <GreenElement>
+        hello
+      </GreenElement>
+      <BlueElement>
+      hello
+      </BlueElement>
       </div>
     );
   }
