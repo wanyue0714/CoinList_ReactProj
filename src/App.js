7f2b8b57a44abd11d3544e2296f6bab50eb81de9
万悦 <wanyue@banetsus-MBP.fios-router.home>
2018-10-23 15:50:02 -0400

    Finish content with dynamic buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 const GreenElement = styled.div`
   color: yellow;
-  font-size: 30px;
+  font-size: 1.3em;
   font-family: 'Mali', cursive;
 `;
 // extend the green element
@@ -13,43 +13,68 @@ const BlueElement = styled(GreenElement)`
 `;
 
 const Logo = styled.div`
-  font-size: 1.5em;
+  font-size: 1.3em;
 `;
 
 const ControlButton = styled.div`
-  font-size: 1.5em;
+  cursor: pointer;
+  font-size: 1.3em;
+  ${props => props.active && css`
+     text-shadow: 0px 0px 30px #03ff03
+     font-size: 1.3em;
+  `}
+  
 `;
 
 const AppLayout = styled.div`
   padding: 40px;
+`;
+
+const Bar = styled.div`
+  margin-down: 40px;
   display: grid;
-  grid-template-columns: 400px auto 200px 100px;
+  grid-template-columns: 400px auto 150px 100px;
+`;
+
+const Content = styled.div`
+ 
 `;
 
 
 
 class App extends Component {
+    state = {
+        page: 'dashboard'
+    }
+    displayingDashboard = () => this.state.page === 'dashboard';
+    displayingSettings = () => this.state.page === 'settings';
   render() {
     return (
-      <AppLayout>
+     <AppLayout>
+      <Bar>
         <Logo>
-           YueYue's home page
+           Home page
         </Logo>
         <div>
         </div>
-        <ControlButton>
-           Dashboard
+        <ControlButton onClick={()=>{this.setState({page: 'dashboard'})}} active = {this.displayingDashboard()}>
+              Dashboard
         </ControlButton>
-          <ControlButton>
+        <ControlButton onClick={()=>{this.setState({page: 'settings'})}} active = {this.displayingSettings()}>
               Settings
-          </ControlButton>
+        </ControlButton>
       <GreenElement>
         Hello, I'm little piggy
       </GreenElement>
       <BlueElement>
        Love you !
       </BlueElement>
-      </AppLayout>
+      </Bar>
+      <Content>
+          Hello, I'm {this.state.page}
+
+      </Content>
+     </AppLayout>
     );
   }
 }
